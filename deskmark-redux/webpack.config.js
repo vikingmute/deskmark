eedaecb8ae32bf6eb8d7f18d48c283b6c17f1632
vikingmute <vikingmute@gmail.com>
2016-02-21 16:54:17 +0800

    改变文件结构，分别放入两个文件夹中

--- deskmark-redux/webpack.config.js
+++ deskmark-redux/webpack.config.js
@@ -3,15 +3,12 @@ var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 
 var ROOT_PATH = path.resolve(__dirname);
-var SRC_PATH = path.resolve(ROOT_PATH, 'src');
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports= {
   entry: {
-    app: path.resolve(SRC_PATH, 'app.jsx')
-    //for redux example
-    //app: path.resolve(APP_PATH, 'index.jsx')
+    app: path.resolve(APP_PATH, 'app.jsx')
   },
   output: {
     path: BUILD_PATH,
@@ -31,8 +28,8 @@ module.exports= {
     extensions: ['', '.js', '.jsx'],
     alias: {
       'node_modules': path.resolve(ROOT_PATH, 'node_modules/'),
-      'utils': path.resolve(SRC_PATH, 'utils/'),
-      'components': path.resolve(SRC_PATH, 'components/')
+      'utils': path.resolve(APP_PATH, 'utils/'),
+      'components': path.resolve(APP_PATH, 'components/')
     }
   },
   module: {
@@ -40,7 +37,7 @@ module.exports= {
       {
         test: /\.jsx?$/,
         loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react'],
-        include: SRC_PATH
+        include: APP_PATH
       },
       {
         test: /\.scss$/,
---
+++ deskmark/webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+module.exports= {
+  entry: {
+    app: path.resolve(APP_PATH, 'app.jsx')
+    //for redux example
+    //app: path.resolve(APP_PATH, 'index.jsx')
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+
+  //enable dev source map
+  devtool: 'eval-source-map',
+  //enable dev server
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'node_modules': path.resolve(ROOT_PATH, 'node_modules/'),
+      'utils': path.resolve(APP_PATH, 'utils/'),
+      'components': path.resolve(APP_PATH, 'components/')
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react'],
+        include: APP_PATH
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Deskmark app'
+    })
+  ]
+}
