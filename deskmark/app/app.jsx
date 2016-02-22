eaf97a71f5e973884786e9359f469189ba502a9a
nighca <nighca@live.cn>
2016-02-22 21:52:02 +0800

    use app folder as root instead of alias config

--- deskmark/app/app.jsx
+++ deskmark/app/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DeskMark from 'components/desk-mark/desk-mark';
 
-import 'node_modules/bootstrap/scss/bootstrap.scss';
+import 'bootstrap/scss/bootstrap.scss';
 
 const app = document.createElement('div');
 document.body.appendChild(app);
