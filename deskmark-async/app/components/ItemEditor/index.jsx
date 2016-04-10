e77a296b7c48a48e8a97ef51e4f65ae72fb37612
nighca <nighca@live.cn>
2016-04-10 19:03:30 +0800

    use functional style for pure component

--- deskmark-async/app/components/ItemEditor/index.jsx
+++ deskmark-async/app/components/ItemEditor/index.jsx
@@ -14,40 +14,34 @@ const propTypes = {
   onCancel: PropTypes.func.isRequired
 };
 
-class Editor extends React.Component {
+function Editor({ item, onSave, onCancel }){
+  item = item || {
+    title: '',
+    content: ''
+  };
 
-  render() {
+  let saveText = item.id ? '保存' : '创建';
 
-    let { item, onSave, onCancel } = this.props;
+  let save = () => {
+    onSave({
+      ...item,
+      title: this.refs.title.value,
+      content: this.refs.content.value
+    });
+  };
 
-    item = item || {
-      title: '',
-      content: ''
-    };
-
-    let saveText = item.id ? '保存' : '创建';
-
-    let save = () => {
-      onSave({
-        ...item,
-        title: this.refs.title.value,
-        content: this.refs.content.value
-      });
-    };
-
-    return (
-      <div className="col-md-8 item-editor-component">
-        <div className="control-area">
-          <button onClick={save} className="btn btn-success">{saveText}</button>
-          <button onClick={onCancel} className="btn secondary">取消</button>
-        </div>
-        <div className="edit-area">
-          <input ref="title" placeholder="请填写标题" defaultValue={item.title} />
-          <textarea ref="content" placeholder="请填写内容" defaultValue={item.content} />
-        </div>
+  return (
+    <div className="col-md-8 item-editor-component">
+      <div className="control-area">
+        <button onClick={save} className="btn btn-success">{saveText}</button>
+        <button onClick={onCancel} className="btn secondary">取消</button>
       </div>
-    );
-  }
+      <div className="edit-area">
+        <input ref="title" placeholder="请填写标题" defaultValue={item.title} />
+        <textarea ref="content" placeholder="请填写内容" defaultValue={item.content} />
+      </div>
+    </div>
+  );
 }
 
 Editor.propTypes = propTypes;
