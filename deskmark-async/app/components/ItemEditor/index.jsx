816b4e201a98c55bc95753efc7f9376858a92de4
nighca <nighca@live.cn>
2016-04-11 20:30:32 +0800

    use class instead of functional (functional component can not have refs)

--- deskmark-async/app/components/ItemEditor/index.jsx
+++ deskmark-async/app/components/ItemEditor/index.jsx
@@ -14,36 +14,40 @@ const propTypes = {
   onCancel: PropTypes.func.isRequired
 };
 
-function Editor({ item, onSave, onCancel }){
-  item = item || {
-    title: '',
-    content: ''
-  };
+class ItemEditor extends React.Component {
+  render() {
+    let { item, onSave, onCancel } = this.props;
 
-  let saveText = item.id ? '保存' : '创建';
+    item = item || {
+      title: '',
+      content: ''
+    };
 
-  let save = () => {
-    onSave({
-      ...item,
-      title: this.refs.title.value,
-      content: this.refs.content.value
-    });
-  };
+    let saveText = item.id ? '保存' : '创建';
 
-  return (
-    <div className="col-md-8 item-editor-component">
-      <div className="control-area">
-        <button onClick={save} className="btn btn-success">{saveText}</button>
-        <button onClick={onCancel} className="btn secondary">取消</button>
+    let save = () => {
+      onSave({
+        ...item,
+        title: this.refs.title.value,
+        content: this.refs.content.value
+      });
+    };
+
+    return (
+      <div className="col-md-8 item-editor-component">
+        <div className="control-area">
+          <button onClick={save} className="btn btn-success">{saveText}</button>
+          <button onClick={onCancel} className="btn secondary">取消</button>
+        </div>
+        <div className="edit-area">
+          <input ref="title" placeholder="请填写标题" defaultValue={item.title} />
+          <textarea ref="content" placeholder="请填写内容" defaultValue={item.content} />
+        </div>
       </div>
-      <div className="edit-area">
-        <input ref="title" placeholder="请填写标题" defaultValue={item.title} />
-        <textarea ref="content" placeholder="请填写内容" defaultValue={item.content} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-Editor.propTypes = propTypes;
+ItemEditor.propTypes = propTypes;
 
-export default Editor;
+export default ItemEditor;
